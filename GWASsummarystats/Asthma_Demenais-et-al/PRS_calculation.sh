624757e0ed49806dc883888a266f82d538fc8203
HeedukOh <90283139+heeduk@users.noreply.github.com>
2025-07-28 12:42:36 -0400

    250728

--- GWASsummarystats/Asthma_Demenais-et-al/PRS_calculation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=PRS
-#SBATCH --output=PRS_%j.out
-
-~/local/bin/plink --bfile 1kgp_merged_nodup \
---score /path/to/GWASsummarystats/Asthma_Demenais-et-al/asthma_allchr.prs 1 2 3 sum \
---out /path/to/GWASsummarystats/Asthma_Demenais-et-al/PRS_results/1kgp_allchr_asthmaPRS
